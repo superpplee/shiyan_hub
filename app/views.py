0134d9917dde9c15b3f5c142e33635b47e8a310c
superpplee <1023512730@qq.com>
2017-06-25 02:47:27 +0800

    Day2 add web table

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 # -*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template,flash,redirect
+
+from forms import LoginForm
 
 from app import app
 
-@app.route('/')
 
-@app.route('/index')
 
 def index():
+
 	user = {'nickname':'Miguel'}
 
 	posts = [
@@ -24,5 +25,20 @@ def index():
 
 	return render_template("index.html",title = 'Home',user = user,posts = posts)
 
+@app.route('/login',methods = ['GEt','POST'])
 
+
+def login():
+	form = LoginForm()	
 	
+	if form.validate_on_submit():
+
+		flash('Login requested for Name: + form.name.data')
+
+		flash('password:' + str(form.password.data))
+
+		flask('remember_me:' + str(form.remember_me.data))
+
+		return redirect('/index')
+	
+	return render_template('login.html',title = 'Sign In',form=form)
