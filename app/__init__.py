fe13576439ad04ce59accd4484ffbf8c48187d01
superpplee <1023512730@qq.com>
2017-06-25 22:42:15 +0800

    add database

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@
 
 from flask import Flask
 
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 app.config.from_object('config')
 
-from app import views
+db = SQLAlchemy(app)
+
+from app import views,models
